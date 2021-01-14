c46592e27971cd0c0dab9b99c4185471bfba6973
Lyubov Zabavina <zabavinal@yandex.ru>
2021-01-14 23:33:23 +0300

    one method

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
         CreditPaymentService service = new CreditPaymentService();
-        long result1 = service.calculate1(1_000_000);
-        System.out.println("Аннуитентный платеж при сроке кредита - 1 год = " + result1);
-
-        long result2 = service.calculate2(1_000_000);
-        System.out.println("Аннуитентный платеж при сроке кредита - 2 года = " + result2);
-
-        long result3 = service.calculate3(1_000_000);
-        System.out.println("Аннуитентный платеж при сроке кредита - 3 года = " + result3);
+        long result = service.calculate(1_000_000, 24);
+        System.out.println("Аннуитетный платеж = " + result);
     }
 }
